20ecea9e27a72280cf22ea53abc7bc2407a9b566
Shermin Sultana <rima64549@gmail.com>
2023-09-26 16:37:19 +0600

    updated pie chart and show percentage

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,41 +1,28 @@
-/* eslint-disable no-undef */
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Legend, Tooltip,Cell, ResponsiveContainer } from 'recharts';
-
-
-import { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Legend, Tooltip, Cell, ResponsiveContainer } from "recharts";
 import { useLoaderData } from "react-router-dom";
 
-
-
 const Statistics = () => {
-    
-   
-   const allData = useLoaderData();
-   const allDataNum = allData.length ;
-   console.log(allDataNum) ;
-   const favoritieDonations = JSON.parse(localStorage.getItem('favorities'));
-   
- const selectedDataNum = favoritieDonations.length ;
- console.log(selectedDataNum) ;
+  const allData = useLoaderData();
+  const allDataNum = allData.length;
+  const favoritieDonations = JSON.parse(localStorage.getItem('favorities'));
+  const selectedDataNum = favoritieDonations.length;
 
+  
+  const percentage = parseFloat(((selectedDataNum / allDataNum) * 100).toFixed(2));
 
- const percentage = (selectedDataNum / allDataNum) * 100;
- const pieChartData = [
+  const pieChartData = [
     { name: 'Total donation', value: percentage },
     { name: 'Your donation', value: 100 - percentage },
   ];
 
-   
-    
   const colors = ['#FF5733', '#4286f4'];
-   
-  
-    return (
-      <div>
- <h1 className='text-center font-bold text-blue-800 text-2xl'>Donation Statistics</h1>
 
-<ResponsiveContainer width="100%" height={400}>
+  return (
+    <div>
+      <h1 className='text-center font-bold text-blue-800 text-2xl'>Donation Statistics</h1>
+
+      <ResponsiveContainer width="100%" height={400}>
         <PieChart>
           <Pie
             dataKey="value"
@@ -44,25 +31,34 @@ const Statistics = () => {
             cy="50%"
             outerRadius={80}
             fill="#8884d8"
-            
-            label>
-                 {pieChartData.map((entry, index) => (
+            label={({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+              const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+              const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
+              const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
+
+              return (
+                <text
+                  x={x}
+                  y={y}
+                  fill="white"
+                  textAnchor="middle"
+                  dominantBaseline="middle"
+                >
+                  {`${(percent * 100).toFixed(2)}%`}
+                </text>
+              );
+            }}
+          >
+            {pieChartData.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
             ))}
-            </Pie>
-          
-          
-         
-          <Tooltip />
+          </Pie>
+          <Tooltip formatter={(value, name) => [`${(value * 100).toFixed(2)}%`, name]} />
           <Legend />
         </PieChart>
       </ResponsiveContainer>
-   
-  
-    
-
-      </div> 
-      );
+    </div>
+  );
 };
 
 export default Statistics;
